f9dfaf655e49fba6704c1f385f3b649456d32d4a
Guest User <Guest@Epicodus-0F.local>
2017-05-18 15:26:14 -0700

    add trigger on hover functionality and bones of trigger on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { TweenLite, TweenMax, TimelineLite, TimelineMax, Power1, Power2, SlowMo, Power0} from "gsap";
+import { Component, OnInit, HostListener, EventEmitter, Directive } from '@angular/core';
+import { TweenLite, TweenMax, TimelineLite, TimelineMax, Power1, Power2, SlowMo, Power0, Sine} from "gsap";
 
 declare var jQuery: any;
 declare var $: any;
@@ -34,4 +34,39 @@ export class AppComponent implements OnInit {
       element.scrollIntoView(alignWithTop);
     }
   }
+
+  hoverInit(e) {
+    let tl = new TimelineMax({repeat:0});
+    if(e.target.id == "p2") {
+      TweenMax.staggerTo("#part1", 10, {y:100, x:100}, 0.3);
+
+      tl.staggerTo("#part2", 4, {className:"+=part2show",  opacity:0, ease:Sine.easeIn}, 2);
+
+      tl.staggerTo(["#elip1", "#elip2", "#elip3"], 0.6, {className:"+=showPart6", opacity:1, ease:Power1.easeIn}, 0.3, "normal");
+
+      tl.staggerTo(['#part3'], 2, {className:"+=part3show", opacity:1, ease:Power1.easeIn}, 2);
+
+      TweenMax.staggerTo("#part4", 10, {y:100, x:100}, 0.3);
+
+      TweenMax.staggerTo("#part5", 10, {y:100, x:100}, 0.3);
+
+    } else if (e.target.id == "p3") {
+      TweenMax.staggerTo('#test2', 50, {y:10, x:2000}, 0.3);
+    }
+  }
+
+  // @HostListener("window:scroll", [])
+  // scrollInit(e) {
+  //   if (e) {
+  //     console.log(e);
+  //     let scrollPos = document.getBoundingClientRect();
+  //     console.log(scrollPos);
+  //     let screenPos = e.clientY;
+  //     console.log(screenPos);
+  //     if (scrollPos == screenPos) {
+  //       alert("works!");
+  //     }
+  //   }
+  //   // console.log("WORKS!");
+  // }
 }
